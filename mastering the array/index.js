20a7e293f874bc760d96ccf92b1ba9ea810039f9
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-07-09 23:57:44 +0600

    second largest element:

--- mastering the array/index.js
+++ mastering the array/index.js
@@ -9,16 +9,38 @@
 
 // max element find  -- without sorting
 
-let arr = [10, 21, 100, 2, 36];
+// let arr = [10, 21, 100, 2, 36];
 
-let max = arr[0];
+// let max = arr[0];
 
-for (let i = 1; i < arr.length; i++) {
-  if (max < arr[i]) {
-    max = arr[i];
-  }
-}
+// for (let i = 1; i < arr.length; i++) {
+//   if (max < arr[i]) {
+//     max = arr[i];
+//   }
+// }
 
-console.log(max);
+// console.log(max);
 
 // 2nd max element find 
+
+let arr = [10,30,40,56,45,36,85,12]
+
+let maximum = Math.max(arr[0],arr[1])
+let secondMaximum = Math.min(arr[0],arr[1])
+
+for(let i = 2; i<arr.length; i++){
+    if(arr[i]> maximum){
+        secondMaximum = maximum
+        maximum = arr[i]
+    } else if(arr[i] > secondMaximum && maximum != arr[i]){
+        secondMaximum = arr[i]
+    }
+}
+
+console.log(secondMaximum)
+
+// reverse the array without extra space
+
+
+
+
