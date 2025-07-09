b1e93c24c0c9d367823e1c99af4698f83fc00f00
Asif Ahmed Sahil <asifahmedsahil.12@gmail.com>
2025-07-09 21:50:16 +0600

    max element find:

--- mastering the array/index.js
+++ mastering the array/index.js
@@ -1,1 +1,24 @@
-// master the array 
+// master the array
+// push == in the array
+// pop == last element out
+// noting in js is fixed , if array is 3 Array(3) then i also put arr[10] = 150
+
+// array is linear ds which store multiple value  in contiuous
+
+// sum of n element of an array
+
+// max element find  -- without sorting
+
+let arr = [10, 21, 100, 2, 36];
+
+let max = arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+  if (max < arr[i]) {
+    max = arr[i];
+  }
+}
+
+console.log(max);
+
+// 2nd max element find 
