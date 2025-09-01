d8da228cbb19b3a1e6b8a7f89809247bf2b68708
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-09-01 23:02:35 +0600

    update:

--- Leetcode/1134_armstrong_number.js
+++ Leetcode/1134_armstrong_number.js
@@ -1,20 +1,15 @@
-function armstrogNumber(n){
-    let num =n
-    let total = 0
-    let numberOfDigit = String(n).length
-    
+function armstrogNumber(n) {
+  let num = n;
+  let total = 0;
+  let numberOfDigit = String(n).length;
 
+  while (num > 0) {
+    let lastDigit = num % 10;
+    total = total + lastDigit ** numberOfDigit;
+    num = Math.floor(num / 10);
+  }
 
-    while (num > 0){
-        let lastDigit = num % 10
-        total = total + (lastDigit ** numberOfDigit )
-        num = Math.floor(num / 10)
-    }
-
-    return total === n
-
+  return total === n;
 }
 
-
-console.log(armstrogNumber(153))
-// update
+console.log(armstrogNumber(153));
